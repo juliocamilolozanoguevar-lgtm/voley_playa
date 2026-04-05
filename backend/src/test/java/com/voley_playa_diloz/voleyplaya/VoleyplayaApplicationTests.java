1eb2b21549b5ca97d182c161dd0b74d9b6485cf6
Julio_Lozano <juliocamilolozanoguevara@gmail.com>
2026-04-05 16:50:37 -0500

    Create VoleyplayaApplicationTests.java

---
+++ backend/src/test/java/com/voley_playa_diloz/voleyplaya/VoleyplayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voley_playa_diloz.voleyplaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoleyplayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
