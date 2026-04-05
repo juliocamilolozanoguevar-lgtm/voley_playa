1a24cb22acdcc606233465e250b77cf719d9f79f
Julio_Lozano <juliocamilolozanoguevara@gmail.com>
2026-04-05 16:50:32 -0500

    Create VoleyplayaApplication.java

---
+++ backend/src/main/java/com/voley_playa_diloz/voleyplaya/VoleyplayaApplication.java
@@ -0,0 +1,13 @@
+package com.voley_playa_diloz.voleyplaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoleyplayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoleyplayaApplication.class, args);
+	}
+
+}
